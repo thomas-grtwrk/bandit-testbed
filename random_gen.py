3f41e335e6acb9e1cde7ea9feeb56d09051f56f9
tbmcoding <tbm247@mun.ca>
2025-06-21 22:37:01 -0400

    add drift

--- random_gen.py
+++ random_gen.py
@@ -18,9 +18,10 @@ all_runs = []
 all_optimal_actions = []
 all_optimal_bandit_options = []
 
-epsilon = 0
-set_optimistic_values = 1
+epsilon = .1
+set_optimistic_values = 0
 gradient_bandit = 0
+drift=1
 alpha = .1
 
 def choose_max(choices):
@@ -33,8 +34,57 @@ def choose_max(choices):
     else:
         return max_indexs[random.randrange(number_of_max)]
     
+def create_drift(steps, reversion=True):
+    
+    mu, sigma = 0, 1.0 # mean and standard deviation
+    initial_reward_values = rg.normal(mu,sigma,10)
+    initial_reward_values = np.array(initial_reward_values)
+    # TODO change optimal bandit according to drift
+    optimal_bandit = initial_reward_values.argmax()
+    
+    bandit_drifts = [rg.normal(0,.01**2,steps) for i in np.arange(0,10,1)]
+    bandit_drifts = np.array(bandit_drifts)
+    
+    bandit_drifts[:,0] = bandit_drifts[:,0] + initial_reward_values
+    if reversion:
+        #print("True")
+        for step in np.arange(1,steps,1):
+            bandit_drifts[:,step] = .5 * bandit_drifts[:,step-1] + bandit_drifts[:,step]
+            #code.interact(local=locals())
+    else:
+        #print("False")
+        for step in np.arange(1,steps,1):
+            bandit_drifts[:,step] = bandit_drifts[:,step-1] + bandit_drifts[:,step]
+    
+    def normal(mu):
+        return rg.normal(mu,1)
+    
+    mu_array = bandit_drifts
+    bandit_drifts = np.vectorize(normal)(bandit_drifts)
+    
+    return bandit_drifts,optimal_bandit,mu_array
 
+def standard_rewards(steps):
+    
+    mu, sigma = 0, 1.0 # mean and standard deviation
+    # Setting actual reward values from normal distribution with 10 bandits
+    actual_reward_values = rg.normal(mu,sigma,10)
+    
+    # Sampling distribution with given actual reward as mean
+    rewards = [rg.normal(mu_i,sigma,steps) for mu_i in actual_reward_values]
+    optimal_bandit = actual_reward_values.argmax()
+    rewards_array = np.array(rewards)
+    return rewards_array,optimal_bandit,actual_reward_values
+    
 for sim in np.arange(0,1000,1):
+    
+    
+    if drift:
+        rewards_array, optimal_bandit,mu_array = create_drift(2000)
+    else:
+        rewards_array, optimal_bandit,mu_array = standard_rewards(2000)
+        
+    """    
     mu, sigma = 0, 1.0 # mean and standard deviation
     # Setting actual reward values from normal distribution with 10 bandits
     actual_reward_values = rg.normal(mu,sigma,10)
@@ -43,7 +93,7 @@ for sim in np.arange(0,1000,1):
     rewards = [rg.normal(mu_i,sigma,2000) for mu_i in actual_reward_values]
     optimal_bandit = actual_reward_values.argmax()
     rewards_array = np.array(rewards)
-    
+    """
     
     greedy_estimates = np.zeros(10)
     choice_estimates = np.zeros(10)
@@ -53,9 +103,7 @@ for sim in np.arange(0,1000,1):
     
     if set_optimistic_values:
         choice_estimates = choice_estimates + np.percentile(rewards_array[optimal_bandit],99.5)
-        #print(choice_estimates)
     
-    #print(choice_estimates)
     steps = np.zeros(10)
     steps = steps 
     
@@ -111,7 +159,6 @@ for sim in np.arange(0,1000,1):
         
         # gradient bandit
         if gradient_bandit:
-            print("Why")
             e_sum = sum(math.e**np.array(gradient_estimates))
             #print("e_sum",e_sum)
             for index in np.arange(0,10,1):
@@ -123,10 +170,8 @@ for sim in np.arange(0,1000,1):
                 else:
                     gradient_estimates[index] = gradient_estimates[index] - alpha*(best_choice-average_award)*soft_max[index]
         
-        #print(choice_estimates)
         #greedy_average_award.append(greedy_estimates[greward_selected])
         choice_average_award.append(best_choice)
-        #code.interact(local=locals())
         
     all_runs.append(choice_average_award)
     all_optimal_actions.append(optimal_action)
